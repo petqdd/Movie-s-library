e1294bca5accbef1f7d51e2a9231eb4db24acc5e
petqd <petq.dankova@gmail.com>
2021-03-25 15:54:02 +0200

    Some changes in live chat with SignalR

--- Web/MovieLibrary.Web/Program.cs
+++ Web/MovieLibrary.Web/Program.cs
@@ -14,6 +14,7 @@
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                     {
+                        webBuilder.UseApplicationInsights();
                         webBuilder.UseStartup<Startup>();
                     });
     }
